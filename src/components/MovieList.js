7fc5f23fc0f34a65dc1734220062b0256638a3fa
boobalan08 <boobalans42@gmail.com>
2023-01-13 17:48:47 +0530

    cc

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -46,7 +46,7 @@ export function MovieList() {
               editButton={
                 <IconButton
                   aria-label="delete"
-                  onClick={() => navigate(`edit/${mv._id}`)}
+                  onClick={() => navigate(`/${mv._id}`)}
                 >
                   <EditIcon color="primary" />
                 </IconButton>
